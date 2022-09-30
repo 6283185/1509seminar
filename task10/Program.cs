2a55caa59b6f4da09fae227c44b8d69d54e97e4f
Dmitry <chernyshov85@mail.ru>
2022-10-01 01:04:41 +0300

    Вывод второго числа из трехзначного

---
+++ task10/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+int numLen = number.ToString().Length;
+if (numLen < 3)
+{
+    Console.WriteLine("Число меньше трёх знаков");
+}
+else if (numLen > 3)
+{
+    Console.WriteLine("Число больше трёх знаков");
+}
+else
+{
+    Console.Write((number % 100) / 10);
+}
